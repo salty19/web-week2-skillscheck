4b1f62b7e240f46e823241e0635a81e47e90f94f
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-14 19:15:26 -0700

    6-inProgress

--- javascript-3/student.js
+++ javascript-3/student.js
@@ -31,13 +31,15 @@ function addItem(item){
 
 // Code Here
 function removeItem(i){
-    groceries.splice(i, i, '')
+    groceries.splice(i, 1,)
     displayData()
 }
 //////////////////STEP 6////////////////////
 //Create a function called 'checkGroceryCount'. Inside of checkGroceryCount, you're going to check the total number of groceries in your groceries array using .length. Using an if/else statement, check if the number of groceries is greater than or equal to 5. If it is, return the string 'That looks like a big trip' from the function. If the number of groceries is equal to one, return the string '1 item'. If the amount of groceries is between 1 and 5, return the string 'NUMBEROFGROCERIES items' with NUMBEROFGROCERIES replaced by the number of items in your groceries array.
 
 // Code Here
-
+function checkGroceryCount(){
+    if()
+}
 //////////////////STEP 6////////////////////
 // Refresh the index.html page. You should now see the same thing you saw when you first loaded the page but you should now be able to add items to your grocery list with a dynamic total showing. If you double click on an item, it should remove that item
